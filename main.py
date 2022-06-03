ff3bd0629caa0ef521fcb1af1d67a09903d9b695
Vicky007-git <106264023+Vicky007-git@users.noreply.github.com>
2022-06-03 10:12:59 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+from turtle import *
+from random import randrange
+from freegames import square, vector, square, snake
+
+food=vector(0,0)
+snack=[vector(10,0)]
+aim=vector(0,-10)
+
+def change(x,y):
+    aim.x=x
+    aim.y=y
+
+def inside(head):
+     return-200<head.x<190 and -200<head.y<190
+
+def move():
+    head='snake'[-1].copy()
+    head.move(aim)
+
+    if not inside(head)or head in 'snake':
+        square(head.x,head.y,9,'red')
+        update()
+        return
+
+    snake.append()
+
+    if head==food:
+        print('snake',len(snake))
+        food.x=randrange(-15,15)*10
+        food.y=randrange(-15,15)*10
+    else:
+        snake.pop(0)
+    clear()
+
+    for body in snack:
+        square(body.x,body.y,9,'green')
+
+
+    square(food.x,food.y,9,'red')
+    update()
+    ontimer(move,100)
+
+    hideturtle()
+    tracer(False)
+    listen()
+    onkey(lambda:change(10,0),'right')
+    onkey(lambda:change(-10,0),'left')
+    onkey(lambda:change(0,10), 'up')
+    onkey(lambda:change(0,-10), 'down')
+    move()
+    done()
